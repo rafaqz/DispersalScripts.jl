00558bc0fc55deb42ceb21e74da9705c82a40d43
Rafael Schouten <rafaelschouten@gmail.com>
2019-01-09 11:05:03 +1100

    better par_a default

--- human.jl
+++ human.jl
@@ -9,7 +9,7 @@ scale = 8
 aggregator = Dispersal.MeanDownsampling()
 human_exponent = 4.0/3.0
 dist_exponent = 1.0
-par_a = 1e-3
+par_a = 1e-5
 shortlist_len = 100
 
 
@@ -17,15 +17,8 @@ shortlist_len = 100
 # @time precalc, props = Dispersal.precalc_human_dispersal(downsampled_human, cellsize, shortlist_len, human_exponent, dist_exponent)
 # @save "usa_precalc.jld" precalc props
 
-@time humandisp = HumanDispersal(human_pop; scale=scale, shortlist_len=shortlist_len, par_a = par_a,
+@time humandisp = HumanDispersal(human_pop; scale=scale, shortlist_len=shortlist_len, par_a=par_a,
                                  cellsize=cellsize, human_exponent=human_exponent, dist_exponent=dist_exponent)
-
-HumanDispersal(human_pop, cellsize, scale, aggregator, 
-               human_exponent, dist_exponent, par_a, shortlist_len, [], [], [])
-HumanDispersal{typeof.((human_pop, cellsize, scale, aggregator, 
-                        human_exponent, dist_exponent, par_a, shortlist_len, [], [], []))...
-              }(human_pop, cellsize, scale, aggregator, human_exponent, dist_exponent, par_a, shortlist_len, [], [], [])
-reconstruct(humandisp, flatten(humandisp))
 
 # Show a single precalc in a Gtk output
 single = Dispersal.populate(humandisp.precalc[20, 20], size(init), scale)
