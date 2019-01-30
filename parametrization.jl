b8030d35e8c0e8bdd43be344aac23fd9719d1758
Rafael Schouten <rafaelschouten@gmail.com>
2019-01-30 20:24:37 +1100

    mvoe parametrizer to Dispersal.jl

--- parametrization.jl
+++ parametrization.jl
@@ -1,114 +1,20 @@
 include("setup.jl")
-using Blink
-using Optim
-using FieldMetadata
+include("growth.jl")
+include("human.jl")
 
-@everywhere using LabelledArrays
-@everywhere using Cellular
-@everywhere import Cellular: store_frame!, show_frame, allocate_frames!, @Ok, @Frames, is_async
-@everywhere import Flatten: flatten
-
-
-@Ok @Frames @everywhere struct SumOutput{DI,SF} <: AbstractArrayOutput{T} where DI <: AbstractOutput disp::DI
-    steps::SF
-end
-
-@everywhere show_frame(output::SumOutput, t::Number) = nothing
-
-# An output that sums frames for a number of frames
-@everywhere SumOutput(frames::AbstractVector, steps::Number, years::Number, disp::AbstractOutput) = begin
-    o = SumOutput{typeof.((frames, disp, steps))...}(frames, [false], disp, steps)
-    allocate_frames!(o, frames[1], 2:years)
-    map(f -> f .= 0, o.frames)
-    o
-end
-# Sums frames on the fly to reduce storage
-@everywhere store_frame!(o::SumOutput, frame, t) = begin
-    sze = size(o[1])
-    # Determine the timestep being summed to
-    ts = year_from_month(o, t)
-    # Add frame to current sum frame
-    for j in 1:sze[2]
-        for i in 1:sze[1]
-            @inbounds o[ts][i, j] += frame[i, j]
-        end
-    end
-    show_frame(o.disp, o[ts], t)
-end
-
-@everywhere year_from_month(o, t) = (t - 1one(t)) ÷ o.steps + one(t)
-
-@everywhere struct Parametriser{OP,M,I,Y,S,R,N,OC,CR}
-    output::OP
-    model::M
-    init::I
-    years::Y
-    steps::S
-    regions::R
-    num_runs::N
-    occurance::OC
-    region_lookup::CR
-end
-
-@everywhere crossentropy = function(y, p, minprob = 1e-9)
-    p = min.(p, 1 - minprob)
-    p = max.(p, minprob)
-    -sum( y  .* log.(p) .+ (ones(size(y)) .- y) .* log.(ones(size(p)) .- p))
-end
-
-@everywhere (p::Parametriser)(params) = begin
-    # Rebuild the model with the current parameters
-    names = fieldnameflatten(p.model.models)
-    println("Parameters: ", collect(zip(names, params)))
-    p.model.models = Flatten.reconstruct(p.model.models, params)
-    # Calculate the timespan
-    tstop = p.years * p.steps
-    s = zeros(Bool, p.regions, p.years)
-    det_thresh = 0.1 # proportion of state infected for detection
-    # Used a labelled array so we don't have to know the index number
-    # fort par_a.
-    # Large par a take forever. Limit the max value.
-    # function to pass back s array (region x year)
-    cumsum = @distributed (+) for i = 1:p.num_runs
-        o = deepcopy(p.output)
-        sim!(o, p.model, p.init; tstop=tstop)
-        for t in 1:p.years
-            for r in 1:p.regions
-
-                s[r, t] = (Base.sum((p.region_lookup .== r) .& (o[t] .> 0)) ./
-                                Base.sum((p.region_lookup .== 6) )) > det_thresh
-            end
-        end
-        val = sum((s .== p.occurance)) / prod(size(p.occurance))
-        println("replicate: ", i, " - accuracy: ", val)
-        s
-    end
-    # then build and array of s array means
-    probs = cumsum ./ p.num_runs
-    loss = crossentropy(p.occurance, probs)
-    println("cross-entropy loss: ", loss, "\n")
-    loss
-end
-
-# Define parametrization values ########################################
+@everywhere using Cellular, Dispersal
+using Blink, Optim, FieldMetadata, Colors, LabelledArrays
 
 minval, maxval = 0.0, 100000.0
 # init[250:280, 50:80] .= maxval / 100
 init .*= maxval
 occurance = convert.(Bool, read(data["state_year_spread"]))
-cell_region = convert.(Int, replace(read(data["x_y_state"]), NaN=>0))[:, :, 1]
-years = size(data["state_year_spread"]))[2]
-steps_per_year = 12
-tstop = years * steps_per_year
-num_runs = 5
-num_regions = maximum(cell_region)
+region_lookup = convert.(Int, replace(read(data["x_y_state"]), NaN=>0))[:, :, 1]
+frames_per_step = 12
+num_replicates = 5
 month = 365.25d/12
 simtimestep = month
 
-# Define model components ##############################################
-include("human.jl")
-
-include("growth.jl")
 # Convert growth arrays to units
 growth_layers = Sequence(popgrowth * d^-1, month);
 growth = SuitabilityExactLogisticGrowth(layers=growth_layers, carrycap=maxval);
@@ -134,22 +40,28 @@ model = Models(humandisp, (popdisp, allee, growth, mask); timestep=simtimestep);
 
 # Outputs if you want to view/play with the model ######################
 
-# output = BlinkOutput(init, model; min=minval, max=maxval);
-# Blink.AtomShell.@dot output.window webContents.setZoomFactor(1.5) # output = REPLOutput{:block}(init)
+truenegativecolor = maskcolor = RGB24(0.1, 0.1, 0.5)
+falsenegativecolor = RGB24(0.5, 0.1, 0.1)
+maskcolor = RGB24(0.0, 0.2, 0.2)
+processor = ColorRegionFit(frames_per_step, occurance, region_lookup, 
+                           truenegativecolor, falsenegativecolor, maskcolor)
+
+# output = BlinkOutput(init, model; store=true, processor=processor, min=minval, max=maxval);
+# Blink.AtomShell.@dot output.window webContents.setZoomFactor(1.0) # output = REPLOutput{:block}(init)
 # sim!(output, model, init; tstop=300)
 
-# output = GtkOutput(init; min=minval, max=maxval, store=false)
-# sim!(output, model, init; tstop=24)
+# output = GtkOutput(init; processor=processor, min=minval, max=maxval, store=false)
+# sim!(output, model, init; tstop=20)
 
 # savegif("usa.gif", output)
 # @time sim!(output, model, init; tstop=timesteps)
-# disp = GtkOutput(init; min=minval, max=maxval*steps_per_year, store=false)
+# disp = GtkOutput(init; min=minval, max=maxval*steps, store=false)
 
 # Set up the parametrizer ##############################################
 
-output = SumOutput(init, steps_per_year, years, Cellular.NullOutput())
-p = Parametriser(output, model, init, years, steps_per_year, num_regions,
-                 num_runs, occurance, cell_region)
+output = SumOutput(init, frames_per_step, steps, Cellular.NullOutput())
+p = RegionParametriser(output, model, init, occurance, region_lookup, 
+                 frames_per_step, num_replicates, detection_threshold)
 # sim!(output, p.model, p.init; tstop=tstop)
 
 # Get array and field names for the model from Flatten
@@ -157,6 +69,8 @@ params = flatten(Vector, model.models)
 pnames = fieldnameflatten(model.models)
 # Make a labelled array so we can ignore the order
 namedparams = LArray{eltype(params),1,pnames}(params)
+
+# Define parametrization values ########################################
 
 # Assign our default parameters to the labelled array
 namedparams.minfounders = 55.0
